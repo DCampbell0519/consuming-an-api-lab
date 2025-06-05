d20c9b44729e9b09f6e7a13d6eb44dd819de15c2
Donovan Campbell <donovan@dlcentertainment.net>
2025-06-04 20:16:08 -0700

    update weather/show

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+////////////////////////
+// MIDDLEWARE
+////////////////////////
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+const fetchResults = async (zipSearch) =>{
+    try{
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipSearch}&appid=${process.env.API_KEY}`;
+        const response = await fetch(apiUrl)
+
+        const data = await response.json()
+
+        return data
+
+    }catch (error){
+        console.log("Danger D-Money, Danger!", error)
+    }
+}
+////////////////////////
+// ROUTES
+////////////////////////
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/weather', async (req, res) => {
+    const zipCode = req.body.zipCode
+    const data = await fetchResults(req.body.zipCode)
+    console.log(data)
+    console.log(req.body)
+    if (!data || data.cod != 200) {
+        return res.render('index.ejs', { results: []});
+    }
+    res.redirect('/weather/show')
+})
+
+app.get('/weather/show', (req, res) => {
+    res.render('show.ejs')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Welcome to the Thunderdome!')
+})
